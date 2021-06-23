b61809a13d4f0fe7d37aa4fbdc87b25e93bd3e8d
chuchengxiang <chuchengxiang@ipfsforce.com>
2021-06-23 20:35:43 +0800

    login have reg

--- pages/login.vue
+++ pages/login.vue
@@ -6,6 +6,7 @@
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
             <a href="">Have an account?</a>
+            <a href="">Need an account?</a>
           </p>
 
           <ul class="error-messages">
